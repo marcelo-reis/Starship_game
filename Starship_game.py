51879a2c551e3fc40c7e7be02e3c397ab10efc2f
Marcelo Alves dos Reis <marcelo-reis@outlook.com>
2018-07-26 21:07:44 -0300

    Update Starship_game.py
    inseridos os métodos:
    - .get_width( )
    - .get_height( )

--- Starship_game.py
+++ Starship_game.py
@@ -1,7 +1,7 @@
 # '''
 # Criado por: Marcelo Reis
-# Data: 25/07/2018
-# Ultima Atualização __/__/__
+# Data: 2018.07.25
+# Ultima Atualização 2018.07.26
 # '''
 
 import pygame
@@ -10,16 +10,17 @@ from sys import exit
 # Para poder usar o método randint
 from random import randint
 
+
 # Algumas imagens podem possuir direitos autorais e estão aqui apenas para teste
-imagem_fundo  = '/.../background01.jpg'
-sprite_rocha1 = '/.../sprite_rock01_35px.png'
-sprite_rocha2 = '/.../sprite_rock02_50px.png'
-sprite_rocha3 = '/.../sprite_rock03_130px.png'
-sprite_nave   = '/.../sprite_spaceship01.png'
+imagem_fundo  = '/Python36-32/Starship_game-master/background02.jpg'
+sprite_rocha1 = '/Python36-32/Starship_game-master/sprite_rock01_35px.png'
+sprite_rocha2 = '/Python36-32/Starship_game-master/sprite_rock02_50px.png'
+sprite_rocha3 = '/Python36-32/Starship_game-master/sprite_rock03_130px.png'
+sprite_nave   = '/Python36-32/Starship_game-master/sprite_spaceship01.png'
 
 # Largura e Altura da tela
-largura = 640
-altura  = 480
+largura = 627
+altura  = 339
 tamanho_tela = (largura, altura)
 
 pygame.init()
@@ -34,22 +35,22 @@ nave   = pygame.image.load(sprite_nave).convert_alpha()
 # cria um objeto Clock(0
 relogio = pygame.time.Clock()
 
-# define a posição inicla e a velocidade das rochas e da nave
-speed_rocha1 = 30
+# define a posição inical e a velocidade das rochas e da nave
+speed_rocha1 = 300
 pos_rocha1_x = -35
-pos_rocha1_y = randint(0, 480)
+pos_rocha1_y = randint(0, altura)
 
 speed_rocha2 = 60
 pos_rocha2_x = -50
-pos_rocha2_y = randint(0, 480)
+pos_rocha2_y = randint(0, altura)
 
-speed_rocha3 = 250
+speed_rocha3 = 200
 pos_rocha3_x = -130
-pos_rocha3_y = randint(0, 480)
+pos_rocha3_y = randint(0, altura)
 
-speed_nave = 200
-pos_nave_x = -100
-pos_nave_y = randint(81, 379)
+speed_nave = 15
+pos_nave_x = -nave.get_width()
+pos_nave_y = randint(81, altura - nave.get_height())
 
 while True:
         # fechar o jogo ao clicar no X da janela
@@ -72,30 +73,31 @@ while True:
         # Movimento da rocha1
         mover_rocha1 = tempo_segundos * speed_rocha1
         pos_rocha1_x -= mover_rocha1
-        if pos_rocha1_x < -35:
-            pos_rocha1_x = 675
-            pos_rocha1_y = randint(0,480)
+        #if pos_rocha1_x < -35:
+        if pos_rocha1_x < -rocha1.get_width():
+            pos_rocha1_x = largura + rocha1.get_width()
+            pos_rocha1_y = randint(0,altura)
         
         # Movimento da rocha2
         mover_rocha2 = tempo_segundos * speed_rocha2
         pos_rocha2_x -= mover_rocha2
-        if pos_rocha2_x < -50:
-            pos_rocha2_x = 675
-            pos_rocha2_y = randint(0,480)
+        if pos_rocha2_x < -rocha2.get_width():
+            pos_rocha2_x = largura + rocha2.get_width()
+            pos_rocha2_y = randint(0,altura)
         
         # Movimento da rocha3
         mover_rocha3 = tempo_segundos * speed_rocha3
         pos_rocha3_x -= mover_rocha3
-        if pos_rocha3_x < -130:
-            pos_rocha3_x = 675
-            pos_rocha3_y = randint(0,480)
+        if pos_rocha3_x < -rocha3.get_width():
+            pos_rocha3_x = largura + rocha3.get_width()
+            pos_rocha3_y = randint(0,altura)
 
         # Movimento da nave
         mover_nave = tempo_segundos * speed_nave
         pos_nave_x += mover_nave
-        if pos_nave_x > 740:
-            pos_nave_x = -100
-            pos_nave_y = randint(81,379)
+        if pos_nave_x > largura + nave.get_width():
+            pos_nave_x = -nave.get_width()
+            pos_nave_y = randint(nave.get_height(),altura - nave.get_height())
 
         # aualiza o display    
         pygame.display.flip()
